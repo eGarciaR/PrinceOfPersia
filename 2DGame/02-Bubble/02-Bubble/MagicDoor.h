04948c3d4025e3e628a14a1e96590b4bb3383b1b
ericgarciaribera <e.garcia.ribera@gmail.com>
2017-04-12 11:06:03 +0200

    MagicDoor

---
+++ 2DGame/02-Bubble/02-Bubble/MagicDoor.h
@@ -0,0 +1,31 @@
+#ifndef _MAGICDOOR_INCLUDE
+#define _MAGICDOOR_INCLUDE
+
+
+#include "Sprite.h"
+#include "TileMap.h"
+
+
+class MagicDoor
+{
+
+public:
+	void init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram);
+	void update(int deltaTime);
+	void render();
+	MagicDoor door();
+	void setTileMap(TileMap *tileMap);
+	void setPosition(const glm::vec2 &pos);
+	void changeAnimation();
+	bool opened();
+
+private:
+	glm::ivec2 tileMapDispl, posDoor;
+	Texture spritesheet;
+	Sprite *sprite;
+	TileMap *map;
+
+};
+
+
+#endif // _MAGICDOOR_INCLUDE
